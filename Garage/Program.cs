647d63f1e3ae2def24f343b25de55ec3ccde63b7
Magnus <minemail@gmail.com>
2020-03-31 10:48:28 +0200

    Add project files.

---
+++ Garage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+
+
+        }
+    }
+}
---
+++ UnitTests/GarageUnitTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GarageUnitTests
+    {
+        [TestMethod]
+        public void CreateGarage()
+        {
+
+            Garage garage = new Garage(20);
+      
+
+        }
+    }
+}
