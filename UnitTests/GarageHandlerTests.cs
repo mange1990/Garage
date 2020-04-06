b5878ef597c14e78218308ffee7d31b62930b509
Magnus <minemail@gmail.com>
2020-04-06 10:53:33 +0200

    Ändrat tester

--- UnitTests/GarageHandlerTests.cs
+++ UnitTests/GarageHandlerTests.cs
@@ -13,6 +13,7 @@ namespace UnitTests
         public void GetVehicleByRegNr_Found_ReturnVehicle()
         {
             GarageHandler g = new GarageHandler();
+            g.BuildGarage(5);
             Bus bus1 = new Bus("gmg482", "Black", 8, 6948, 25);
             Bus bus2 = new Bus("krd124", "red", 6, 5928, 10);
             Car car1 = new Car("ABC148", "yellow", 1021, "Porsche");
@@ -25,8 +26,8 @@ namespace UnitTests
             g.AddVehicle(car1);
             g.AddVehicle(car2);
 
-            Vehicle actua11 = g.GetVehicleByRegNr("krd124");
-            Vehicle actual2 = g.GetVehicleByRegNr("grI492");
+            Vehicle actua11 = g.GetVehicleByRegNr("kRD124");
+            Vehicle actual2 = g.GetVehicleByRegNr("gRI492");
 
             Assert.AreEqual(expected1, actua11);
             Assert.AreEqual(expected2, actual2);
@@ -61,6 +62,7 @@ namespace UnitTests
         public void ListVehicleTypeCount_FourVehicle_ReturnStringOfTypeAndCount()
         {
             GarageHandler g = new GarageHandler();
+            g.BuildGarage(5);
             Bus bus1 = new Bus("gmg482", "Black", 8, 6948, 25);
             Bus bus2 = new Bus("krd124", "red", 6, 5928, 10);
             Car car1 = new Car("ABC148", "yellow", 1021, "Porsche");
@@ -73,7 +75,7 @@ namespace UnitTests
 
             string actua1 = g.ListVehicleTypeCount();
 
-            Assert.AreEqual("", actua1);
+            Assert.AreEqual($"Det finns 2 Bus i garaget just nu\nDet finns 2 Car i garaget just nu\n", actua1);
 
 
         }
